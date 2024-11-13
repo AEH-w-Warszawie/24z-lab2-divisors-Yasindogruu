e3245302608ad1dc8c5aa239e42bc769a841aecf
Yasindogruu <dogruya2542_aehit@students.vizja.pl>
2024-11-13 21:41:04 +0100

    new

--- DivisorsLib/DivisorsLib.cpp
+++ DivisorsLib/DivisorsLib.cpp
@@ -4,10 +4,12 @@
 std::vector<unsigned long long> divisors(unsigned long long number)
 {
 	std::vector<unsigned long long> result;
-	for (unsigned long long i = 1; i <= number; ++i)
+	for (unsigned long long i = 1; i <= sqrt(number); ++i)
 	{
 		if (number % i == 0)
 			result.push_back(i);
+		if (i != number / i)
+			result.push_back(number / i);
 	}
 	return result;
 }
